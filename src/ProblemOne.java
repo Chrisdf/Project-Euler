503242088552e41f75ed19fe4c5cf7568047cd6f
Chrisdf <chrisfrantz96@gmail.com>
2014-09-10 12:20:54 -0500

    Solve first problem

--- src/ProblemOne.java
+++ src/ProblemOne.java
@@ -3,8 +3,19 @@ public class ProblemOne {
 
 	public static void main(String[] args) {
 		
+		int count = 0;
 		
+		for(int i = 0; i<1000; i++){
+			
+			if(i%5 == 0)
+				count = count + i;
+			
+			else if(i%3 == 0)
+				count = count + i;
+			
+		}
 		
+		System.out.println(count);
 		
 	}
 
